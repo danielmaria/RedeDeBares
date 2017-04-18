edd1cf2ce300152e32b63ed1c034a12645fe3d0c
danielmaria <danielmariadasilva@gmail.com>
2017-04-18 20:58:02 -0300

    Alteracao necessaria para salvar arquivo e novo arquivo de texto para salvar o historico de entradas e saidas do bar

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -19,18 +19,22 @@ public class Conexao {
 		HashMap<String, ClienteDTO> clientesCadastrados = new HashMap<>();
 		try {
 			FileReader fileReader = new FileReader("ArquivosDeDados\\TabelaCliente.txt");
-			scanner = new Scanner(fileReader).useDelimiter("\\s*#####\\s*|\n");
+			scanner = new Scanner(fileReader).useDelimiter("\\s*#####\\s*");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		while (scanner.hasNext()) {
-			String nome = scanner.next();
-			String cpf = scanner.next();
-			int idade = Integer.parseInt(scanner.next());
-			Genero sexo = Genero.masculinoOuFemino(scanner.next());
-			int id = Integer.parseInt(scanner.next());
-			ClienteDTO cliente = new ClienteDTO(nome, cpf, idade, sexo, id);
-			clientesCadastrados.put(cpf, cliente);
+		while (scanner.hasNextLine()) {
+			if (scanner.hasNext()) {
+				String nome = scanner.next();
+				String cpf = scanner.next();
+				int idade = Integer.parseInt(scanner.next());
+				Genero sexo = Genero.masculinoOuFemino(scanner.next());
+				int id = Integer.parseInt(scanner.next());
+				ClienteDTO cliente = new ClienteDTO(nome, cpf, idade, sexo, id);
+				clientesCadastrados.put(cpf, cliente);
+			} else {
+				break;
+			}
 		}
 		return clientesCadastrados;
 	}
@@ -58,7 +62,7 @@ public class Conexao {
 		stringBuilder.append("#####");
 		
 		stringBuilder.append(cliente.getNrSocio());
-		stringBuilder.append("\n");
+		stringBuilder.append("#####");
 		
 		gravar.append(stringBuilder);
 		gravar.close();
